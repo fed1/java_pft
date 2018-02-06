e88f997d2a70f6afcb8030fb0b3d666c97bb9816
fed1 <nadyafedortsova@gmail.com>
2018-02-06 22:59:27 +0300

    Задание 2

--- sandbox/src/main/java/by/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/by/stqa/pft/sandbox/MyFirstProgram.java
@@ -2,18 +2,14 @@ package by.stqa.pft.sandbox;
 
 public class MyFirstProgram {
 
-public static void main (String [] args){
+   public static void main (String [] args){
 
-    Point p1 = new Point(-14);
-    Point p2 = new Point(-35);
+       Point p1 = new Point(5,6);
+       Point p2 = new Point(14,-76);
+       Point p3 = new Point(-123,-0.78);
 
-
-    System.out.println("Дистанция между точками = " + distance(p1, p2));
-
-}
-
-   public static double distance(Point p1, Point p2){
-
-    return Math.sqrt(Math.pow(p1.pointValue,2)+ Math.pow(p2.pointValue,2));
-   }
+       System.out.println("Дистанция между точками = " + p1.distance()
+                                                        + "; " + p2.distance()
+                                                        + "; " + p3.distance());
+       }
 }
--- sandbox/src/main/java/by/stqa/pft/sandbox/Point.java
+++ sandbox/src/main/java/by/stqa/pft/sandbox/Point.java
@@ -2,9 +2,22 @@ package by.stqa.pft.sandbox;
 
 public class Point {
 
-    public double pointValue;
+    public double pointX;
+    public double pointY;
 
-    public Point(double pointValue) {
-        this.pointValue = pointValue;
+    public Point(double pointX, double pointY) {
+        this.pointX = pointX;
+        this.pointY = pointY;
+    }
+
+    public Point() {
+    }
+
+    public  double distance(){
+
+        this.pointX = pointX;
+        this.pointY = pointY;
+
+        return Math.sqrt(Math.pow(pointX,2)+ Math.pow(pointY,2));
     }
 }
