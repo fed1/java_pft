a040a11df619f5bd09e2e2f0ce6320dee32ff036
fed1 <nadyafedortsova@gmail.com>
2018-02-15 00:25:28 +0300

    Выделение вспом.методов у теста по доб.контакта, параметризация метода доб.контакта

---
+++ addressbook-web-tests/src/test/java/by/stqa/pft/addressbook/ContactData.java
@@ -0,0 +1,31 @@
+package by.stqa.pft.addressbook;
+
+public class ContactData {
+    private final String contactName;
+    private final String contactLastname;
+    private final String contactHomePhone;
+    private final String contactEmail;
+
+    public ContactData(String contactName, String contactLastname, String contactHomePhone, String contactEmail) {
+        this.contactName = contactName;
+        this.contactLastname = contactLastname;
+        this.contactHomePhone = contactHomePhone;
+        this.contactEmail = contactEmail;
+    }
+
+    public String getContactName() {
+        return contactName;
+    }
+
+    public String getContactLastname() {
+        return contactLastname;
+    }
+
+    public String getContactHomePhone() {
+        return contactHomePhone;
+    }
+
+    public String getContactEmail() {
+        return contactEmail;
+    }
+}
