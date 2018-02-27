49c082acfe9b03d60f24db8796c067a5110b39f9
fed1 <nadyafedortsova@gmail.com>
2018-02-27 22:50:29 +0300

    Реализованы тесты на сравнение размера списка групп

--- addressbook-web-tests/src/test/java/by/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/by/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -6,17 +6,17 @@ import org.openqa.selenium.By;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import java.util.List;
+
 
 public class GroupCreationTests extends TestBase{
 
     @Test
     public void testGroupCreation() {
         app.getNavigationHelper().gotoGroupPage();
-        int before = app.getGroupHelper().getGroupCount();
+        List <GroupData> before = app.getGroupHelper().getGroupList();
         app.getGroupHelper().createGroup(new GroupData("test1", null, null));
-        int after = app.getGroupHelper().getGroupCount();
-        Assert.assertEquals(after, before + 1);
-
-    }
-
+        List <GroupData> after = app.getGroupHelper().getGroupList();
+        Assert.assertEquals(after.size(), before.size() + 1);
+        }
 }
--- addressbook-web-tests/src/test/java/by/stqa/pft/addressbook/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/by/stqa/pft/addressbook/tests/GroupModificationTests.java
@@ -2,8 +2,11 @@ package by.stqa.pft.addressbook.tests;
 
 
 import by.stqa.pft.addressbook.model.GroupData;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import org.openqa.selenium.By;
+
+import java.util.List;
 
 public class GroupModificationTests extends TestBase {
 
@@ -13,11 +16,13 @@ public class GroupModificationTests extends TestBase {
         if (! app.getGroupHelper().isThereAGroup()){
             app.getGroupHelper().createGroup(new GroupData("test1", null, null));
         }
-
-        app.getGroupHelper().selectGroup(0);
+        List<GroupData> before = app.getGroupHelper().getGroupList();
+        app.getGroupHelper().selectGroup(before.size()-1);
         app.getGroupHelper().initGroupModification();
         app.getGroupHelper().fillGroupForm(new GroupData("test1", "test077-1", "test0456-1"));
         app.getGroupHelper().submitGroupModification();
         app.getGroupHelper().returnToGroupPage();
+        List<GroupData> after = app.getGroupHelper().getGroupList();
+        Assert.assertEquals(after.size(), before.size());
     }
 }
