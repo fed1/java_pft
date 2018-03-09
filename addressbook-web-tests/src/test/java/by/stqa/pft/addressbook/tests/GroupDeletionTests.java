0ef575e05966fead0f8ab1c416b9de983977dd2f
fed1 <nadyafedortsova@gmail.com>
2018-03-09 18:33:37 +0300

    Добавили аннотацию @BeforeMethod  в тестах для Групп

--- addressbook-web-tests/src/test/java/by/stqa/pft/addressbook/tests/GroupDeletionTests.java
+++ addressbook-web-tests/src/test/java/by/stqa/pft/addressbook/tests/GroupDeletionTests.java
@@ -2,6 +2,7 @@ package by.stqa.pft.addressbook.tests;
 
 import by.stqa.pft.addressbook.model.GroupData;
 import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import java.util.Comparator;
@@ -9,13 +10,16 @@ import java.util.List;
 
 public class GroupDeletionTests extends TestBase {
 
-    @Test
-    public void testGroupDeletion() {
-
+    @BeforeMethod
+    public void ensurePreconditions(){
         app.gotoGroupPage();
         if (! app.getGroupHelper().isThereAGroup()){
             app.getGroupHelper().createGroup(new GroupData("test1", null, null));
         }
+    }
+
+    @Test
+    public void testGroupDeletion() {
         List<GroupData> before = app.getGroupHelper().getGroupList();
         app.getGroupHelper().selectGroup(before.size()-1);
         app.getGroupHelper().deleteSelectedGroups();
@@ -28,7 +32,7 @@ public class GroupDeletionTests extends TestBase {
         before.sort(byId);
         after.sort(byId);
         Assert.assertEquals(before, after);
-       
+
         }
 
 
--- addressbook-web-tests/src/test/java/by/stqa/pft/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/by/stqa/pft/addressbook/tests/TestBase.java
@@ -14,8 +14,8 @@ import org.testng.annotations.BeforeSuite;
 public class TestBase {
 
     protected static final ApplicationManager app = new ApplicationManager(BrowserType.FIREFOX);
-    //protected final ApplicationManager app = new ApplicationManager(BrowserType.IE);
-   // protected final ApplicationManager app = new ApplicationManager(BrowserType.CHROME);
+    //protected static final ApplicationManager app = new ApplicationManager(BrowserType.IE);
+   // protected static final ApplicationManager app = new ApplicationManager(BrowserType.CHROME);
 
     @BeforeSuite
     public void setUp() throws Exception {
